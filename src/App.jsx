798696a310e864032f6260eb1ed94df0a560131d
luchesigerman@gmail.com <luchesigerman@mail.com>
2024-03-14 20:35:15 -0300

    iniciando proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import './App.css'
+import { NavBar } from "./components/NavBar"
+
+function App() {
+  
+  return <Navbar />
+  
+};
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar= () => {
+    return (
+        <>
+        <h5>Geros</h5>
+        <ul>
+            <li><a href="#">Notebooks</a></li>
+            <li><a href="#">Tablets</a></li>
+            <li><a href="#">Celulares</a></li>
+        </ul>
+    
+        </>
+    )
+}
